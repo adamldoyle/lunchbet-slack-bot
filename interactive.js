8d64387ba1dcf0a416cf0edbbb1b2321e38c4ea8
Adam Doyle <adamldoyle@gmail.com>
2020-09-13 19:52:46 -0500

    More debug statements

--- interactive.js
+++ interactive.js
@@ -11,5 +11,9 @@ export const main = handler(async (event) => {
 
   const payload = parse(event.body);
   debug('parsed body', payload);
+  debug('dot type', payload.type);
+  debug('array type', payload['type']);
+  debug('dot callback_id', payload.callback_id);
+  debug('array callback_id', payload['callback_id']);
   return interactiveHandler(payload);
 });
