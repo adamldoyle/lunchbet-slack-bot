8198d0f849a289eb9592fcd7562e871f7aee9e14
Adam Doyle <adamldoyle@gmail.com>
2020-09-13 22:08:28 -0500

    Remove debugs

--- interactive.js
+++ interactive.js
@@ -1,7 +1,6 @@
 import { parse } from 'querystring';
 import { verifyRequest } from './libs/slack';
 import handler from './libs/handler';
-import debug from './libs/debug';
 import interactiveHandler from './interactiveHandler';
 
 export const main = handler(async (event) => {
@@ -9,16 +8,6 @@ export const main = handler(async (event) => {
     throw new Error('Invalid request');
   }
 
-  const payload = parse(event.body);
-  debug('type', typeof payload);
-  debug('payload', payload.payload);
-  debug('keys', Object.keys(payload));
-  debug('json', JSON.stringify(payload));
-  debug('parsed body', payload);
-  debug('who knows', JSON.parse(JSON.stringify(payload)).type);
-  debug('dot type', payload.type);
-  debug('array type', payload['type']);
-  debug('dot callback_id', payload.callback_id);
-  debug('array callback_id', payload['callback_id']);
+  const payload = parse(event.body).payload;
   return interactiveHandler(payload);
 });
