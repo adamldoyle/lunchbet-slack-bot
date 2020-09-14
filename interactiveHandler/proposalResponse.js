87034ffdcd7be16073c238f7b9d5bed77fd896ee
Adam Doyle <adamldoyle@gmail.com>
2020-09-13 23:32:26 -0500

    Overwrite message

--- interactiveHandler/proposalResponse.js
+++ interactiveHandler/proposalResponse.js
@@ -5,6 +5,7 @@ import dynamodb from '../libs/dynamodb';
 export default async function (payload) {
   const response = payload.actions[0].value;
   const betId = payload.callback_id.split(`${types.PROPOSAL_RESPONSE}_`)[1];
+  const newStatus = response === 'accept' ? status.ACCEPTED : status.DECLINED;
   const params = {
     TableName: process.env.tableName,
     Key: {
@@ -12,8 +13,23 @@ export default async function (payload) {
     },
     UpdateExpression: 'SET betStatus = :betStatus',
     ExpressionAttributeValues: {
-      ':betStatus': response === 'accept' ? status.ACCEPTED : status.DECLINED,
+      ':betStatus': newStatus,
     },
   };
   await dynamodb.update(params);
+
+  return {
+    ...payload.original_message,
+    blocks: [
+      ...payload.original_message.blocks,
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `You ${newStatus}`,
+        },
+      },
+    ],
+    attachments: null,
+  };
 }
