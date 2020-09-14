6ab2f24127556baf259d44269fc48e4d6a446115
Adam Doyle <adamldoyle@gmail.com>
2020-09-13 22:38:55 -0500

    Fix build

--- interactive.js
+++ interactive.js
@@ -2,7 +2,6 @@ import { parse } from 'querystring';
 import { verifyRequest } from './libs/slack';
 import handler from './libs/handler';
 import interactiveHandler from './interactiveHandler';
-import debug from './libs/debug';
 
 export const main = handler(async (event) => {
   if (!verifyRequest(event)) {
